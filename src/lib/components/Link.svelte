30016cecd02c0f145ee4a3a7d12673bbb3bd9464
Josh Corbett <joshwcorbett@icloud.com>
2023-07-18 08:56:39 -0600

    fix: :bug: server error when changing locale with absolute urls

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -6,7 +6,8 @@ export let href: string
 $: currentPath = $page.url
 $: ({ selectedLocale, layout } = $page.data)
 $: locale = `${String(selectedLocale.language)?.toLocaleLowerCase()}-${String(selectedLocale.country)?.toLocaleLowerCase()}` ?? 'en-us'
-$: url = locale === 'en-us' ? new URL(href, currentPath) : new URL(`/${locale}${href}`)
+$: isAbsoluteUrl = href.startsWith('http://') || href.startsWith('https://')
+$: url = locale === 'en-us' ? new URL(href, currentPath) : (isAbsoluteUrl ? new URL(href) : new URL(`/${locale}${href}`, currentPath))
 $: shopHost = new URL(layout.shop.primaryDomain.url).host
 
 // remove shop host from url if it's there
